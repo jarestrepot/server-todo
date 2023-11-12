b805272d2eb177aca421573e41454f1d02bae4e3
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2023-11-12 22:19:52 +0100

    .env from the database.

--- src/db/conection.ts
+++ src/db/conection.ts
@@ -1,11 +1,11 @@
 import { Sequelize } from 'sequelize';
+require('dotenv').config();
 
 const db = process.env.MYSQL_ADDON_DB ?? 'todolistapp';
 const root = process.env.MYSQL_ADDON_USER ?? 'root';
 const password = process.env.MYSQL_ADDON_PASSWORD ?? 'aplicaciones';
 const hotsBD = process.env.MYSQL_ADDON_HOST ?? 'localhost';
 const portDB = process.env.MYSQL_ADDON_PORT ?? 3306
-
 
 const sequelizeConnect = new Sequelize(db, root, password, {
   host: hotsBD,
