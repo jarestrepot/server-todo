93a9be145567e39c3e194e5bb8c050cf2a92157b
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2023-11-12 21:44:07 +0100

    Adding guardian to routes. - The user token must be sent for task actions.

--- src/routes/user/route.ts
+++ src/routes/user/route.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 import { UserServiceApp } from '../../controllers/user/userController';
+import { checkAuth } from '../../helpers/auth';
 
 const router =  Router();
 
 router.post('/login', UserServiceApp.loginUser );
 router.post('/register', UserServiceApp.createUser);
-router.post('/newTask/:id', UserServiceApp.newTask);
-router.delete('/deleteTask/:id', UserServiceApp.deleteTask);
-router.patch('/updateTask/:id', UserServiceApp.updateTask);
-router.get('/task/:id', UserServiceApp.getTask);
-router.patch('/modify/:id', UserServiceApp.modifyUser);
-router.delete('/delete/:id', UserServiceApp.deleteUser);
+router.post('/newTask/:id', checkAuth, UserServiceApp.newTask);
+router.delete('/deleteTask/:id', checkAuth, UserServiceApp.deleteTask);
+router.patch('/updateTask/:id', checkAuth, UserServiceApp.updateTask);
+router.get('/task/:id', checkAuth, UserServiceApp.getTask);
+router.patch('/modify/:id', checkAuth, UserServiceApp.modifyUser);
+router.delete('/delete/:id', checkAuth, UserServiceApp.deleteUser);
 
 
 export default router;
